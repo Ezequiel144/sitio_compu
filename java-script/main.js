102d75b9b3dd85ad764c96941f723156d5f589c3
Ezequiel <ezgarcia@frba.utn.edu.ar>
2023-01-27 11:22:26 -0300

    5 commit

--- java-script/main.js
+++ java-script/main.js
@@ -3,12 +3,12 @@ const menu = document.querySelector(".menu");
 const menu_close = document.querySelector(".menu__close");
 
 menu_hamburg.addEventListener("click",()=>{
-    menu.style.transform = "translate(0%)";
+    /* menu.style.transform = "translate(0%)"; */
     menu.style.display = "block";
 });
 
 menu_close.addEventListener("click",()=>{
     
-    menu.style.transform = "translate(110%)";
+/*     menu.style.transform = "translate(110%)"; */
     menu.style.display = "none";
 });
